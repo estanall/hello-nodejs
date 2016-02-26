4d20007e6df16b9efe5425575e140190a9d96068
Conor DuBois <conor@mobileigniter.com>
2016-02-26 11:41:19 -0600

    another test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var riffle = require('jsriffle');
+var fs = require('fs');
 
 riffle.setFabric(process.env.WS_URL);
 riffle.setLogLevelInfo();
@@ -7,6 +8,14 @@ var domain = riffle.Domain(process.env.DOMAIN);
 
 domain.onJoin = function() {
     console.log("ws:", process.env.WS_URL, "domain:", process.env.DOMAIN);
+    fs.readFile('./test.txt', function(err, fileData) {
+        if(err) {
+            return console.log("file read error:", err);
+        }
+
+        console.log(fileData.toString().replace(/ing/g, "ssssssss"));
+    });
+    
     this.register("echo", riffle.want(function(msg) {
         console.log("Echo: " + msg);
         return msg;
