3ed27363827a2ccdf377175cc0be139afe0f6455
Conor DuBois <conor@mobileigniter.com>
2016-02-26 10:55:39 -0600

    test

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ riffle.setLogLevelInfo();
 var domain = riffle.Domain(process.env.DOMAIN);
 
 domain.onJoin = function() {
+    console.log("ws:", process.env.WS_URL, "domain:", process.env.DOMAIN);
     this.register("echo", riffle.want(function(msg) {
         console.log("Echo: " + msg);
         return msg;
     }, String));
-}
+};
 
-domain.join()
+domain.join();
